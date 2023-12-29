312fe4c706172536c2e357c3c2584abe177769cc
Igor Verkhov <iVerkhov@icoud.com>
2023-12-29 19:10:28 +0300

    New version of the course

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,6 +7,22 @@
 
 import SwiftUI
 
+// 1. Create Custom Modifier
+struct BlueTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+// 2. Add simple way to use modifier using extension to View
+extension View {
+    func myCustomed() -> some View {
+        modifier(BlueTitle())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -14,6 +30,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .myCustomed()
         }
         .padding()
     }
